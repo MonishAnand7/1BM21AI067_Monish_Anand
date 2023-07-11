72d0ac4653c0dbee7eb6f8a6d0294c8fd8bdfb55
Monish Anand <137139542+MonishAnand7@users.noreply.github.com>
2023-07-11 10:02:09 +0530

    Create Merge Sort.cpp

---
+++ Merge Sort.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Merge two sorted subarrays into one sorted array
+void merge(vector<int>& arr, int left, int middle, int right) {
+    int n1 = middle - left + 1;
+    int n2 = right - middle;
+
+    // Create temporary arrays
+    vector<int> L(n1), R(n2);
+
+    // Copy data to temporary arrays
+    for (int i = 0; i < n1; i++)
+        L[i] = arr[left + i];
+    for (int j = 0; j < n2; j++)
+        R[j] = arr[middle + 1 + j];
+
+    // Merge the temporary arrays back into arr
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        } else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    // Copy the remaining elements of L[]
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+
+    // Copy the remaining elements of R[]
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+// Merge Sort function
+void mergeSort(vector<int>& arr, int left, int right) {
+    if (left < right) {
+        int middle = left + (right - left) / 2;
+
+        // Sort first and second halves
+        mergeSort(arr, left, middle);
+        mergeSort(arr, middle + 1, right);
+
+        // Merge the sorted halves
+        merge(arr, left, middle, right);
+    }
+}
+
+// Utility function to print an array
+void printArray(const vector<int>& arr) {
+    int size = arr.size();
+    for (int i = 0; i < size; i++)
+        cout << arr[i] << " ";
+    cout << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    cout << "Original array: ";
+    printArray(arr);
+
+    mergeSort(arr, 0, n - 1);
+
+    cout << "Sorted array: ";
+    printArray(arr);
+
+    return 0;
+}
