078ffc4c01cd052c411c27ac598963e0de67b0df
Monish Anand <137139542+MonishAnand7@users.noreply.github.com>
2023-07-11 10:17:06 +0530

    Update Merge Sort.cpp

--- Merge Sort.cpp
+++ Merge Sort.cpp
@@ -3,21 +3,17 @@
 
 using namespace std;
 
-// Merge two sorted subarrays into one sorted array
 void merge(vector<int>& arr, int left, int middle, int right) {
     int n1 = middle - left + 1;
     int n2 = right - middle;
 
-    // Create temporary arrays
     vector<int> L(n1), R(n2);
 
-    // Copy data to temporary arrays
     for (int i = 0; i < n1; i++)
         L[i] = arr[left + i];
     for (int j = 0; j < n2; j++)
         R[j] = arr[middle + 1 + j];
 
-    // Merge the temporary arrays back into arr
     int i = 0, j = 0, k = left;
     while (i < n1 && j < n2) {
         if (L[i] <= R[j]) {
@@ -30,14 +26,12 @@ void merge(vector<int>& arr, int left, int middle, int right) {
         k++;
     }
 
-    // Copy the remaining elements of L[]
     while (i < n1) {
         arr[k] = L[i];
         i++;
         k++;
     }
 
-    // Copy the remaining elements of R[]
     while (j < n2) {
         arr[k] = R[j];
         j++;
@@ -45,7 +39,6 @@ void merge(vector<int>& arr, int left, int middle, int right) {
     }
 }
 
-// Merge Sort function
 void mergeSort(vector<int>& arr, int left, int right) {
     if (left < right) {
         int middle = left + (right - left) / 2;
@@ -59,7 +52,6 @@ void mergeSort(vector<int>& arr, int left, int right) {
     }
 }
 
-// Utility function to print an array
 void printArray(const vector<int>& arr) {
     int size = arr.size();
     for (int i = 0; i < size; i++)
